55f052cca9f0d690ac87185d604431e618112850
Akira Naru Takizawa <akira@hokage.de>
2019-07-18 09:54:13 +0200

    Import __all__ um ACL erweitert Setup, Lizenz hinzugefügt

--- nethandler/__init__.py
+++ nethandler/__init__.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 """Net handler for communication between server and clients."""
+from .acl import AclIp, AclIpGroup
 from .client import CmdClient
 from .server import CmdServer, CmdHandler
 __all__ = [
+    "AclIp", "AclIpGroup",
     "CmdClient",
     "CmdServer", "CmdHandler"
 ]
@@ -10,4 +12,4 @@ __author__ = "Sven Sager"
 __copyright__ = "Copyright (C) 2019 Sven Sager"
 __license__ = "GPLv3"
 __name__ = "nethandler"
-__version__ = "0.0.1"
+__version__ = "0.1.0"
---
+++ setup.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Setup script for nethandler module."""
+__author__ = "Sven Sager"
+__copyright__ = "Copyright (C) 2019 Sven Sager"
+__license__ = "LGPLv3"
+from setuptools import setup
+
+setup(
+
+    packages=['nethandler'],
+    python_requires="~=3.4",
+    keywords="network command socket",
+
+    # Additional meta-data
+    name='nethandler',
+    version='0.1.0',
+    author='Sven Sager',
+    author_email='akira@hokage.de',
+    maintainer="Sven Sager",
+    maintainer_email="akira@hokage.de",
+    url='https://revpimodio.org',
+    description='Call functions on an server',
+    long_description="",
+    download_url="",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        # "Development Status :: 2 - Pre-Alpha",
+        # "Development Status :: 3 - Alpha",
+        # "Development Status :: 4 - Beta",
+        # "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Networking",
+    ],
+    license='LGPLv3',
+)
